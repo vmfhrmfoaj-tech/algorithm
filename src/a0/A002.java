268eed00c7c66d54e8dd9baf53c202bc08011601
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-08-27 19:01:50 +0900

    A002임시저장

--- src/a0/A002.java
+++ src/a0/A002.java
@@ -1,9 +1,41 @@
 package a0;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class A002 {
 
     public boolean solution(String[] phone_book) {
-        return false;
+
+        Comparator<String> comparator = (x, y) -> {
+            int len = x.length() - y.length();
+            if(len < 0) return -1;
+            else if(len > 0 ) return 1;
+            else return x.compareTo(y);
+        };
+
+        String[] sortedArray = Arrays.asList(phone_book).stream()
+                                    .sorted(comparator)
+                                    .toArray(String[]::new);
+
+
+        System.out.println( Arrays.asList(sortedArray).stream().collect(Collectors.joining(", ")) );
+        
+        
+
+        int len = phone_book.length;
+        for(int i=0; i<len-1; i++){
+            String cur = sortedArray[i];
+
+            for(int j=i+1; j<len; j++){
+
+                System.out.println( cur + ", " + sortedArray[j] + " -> "+ sortedArray[j].indexOf(cur));
+                if( sortedArray[j].indexOf(cur) == 0 )
+                    return false;
+            }
+        }
+        return true;
     }
 
 }
