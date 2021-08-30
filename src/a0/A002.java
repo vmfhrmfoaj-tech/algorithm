2cc20ab78846e46d610310f2194bfa2e8ef41950
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-08-30 17:26:36 +0900

    A002해결

--- src/a0/A002.java
+++ src/a0/A002.java
@@ -2,37 +2,32 @@ package a0;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.stream.Collectors;
 
 public class A002 {
 
     public boolean solution(String[] phone_book) {
 
-        Comparator<String> comparator = (x, y) -> {
-            int len = x.length() - y.length();
-            if(len < 0) return -1;
-            else if(len > 0 ) return 1;
-            else return x.compareTo(y);
-        };
+        // Arrays.sort(phone_book, new Comparator<String>(){
 
-        String[] sortedArray = Arrays.asList(phone_book).stream()
-                                    .sorted(comparator)
-                                    .toArray(String[]::new);
+        //     @Override
+        //     public int compare(String x, String y) {
+        //             int len = x.length() - y.length();
+        //             if(len < 0) return -1;
+        //             else if(len > 0 ) return 1;
+        //             else return x.compareTo(y);
+        //     }
+        // });
 
+        Arrays.sort(phone_book);
 
-        System.out.println( Arrays.asList(sortedArray).stream().collect(Collectors.joining(", ")) );
-        
+        System.out.println();
+        Arrays.asList(phone_book).stream().forEach(System.out::println);
         
 
         int len = phone_book.length;
         for(int i=0; i<len-1; i++){
-            String cur = sortedArray[i];
-
-            for(int j=i+1; j<len; j++){
-
-                System.out.println( cur + ", " + sortedArray[j] + " -> "+ sortedArray[j].indexOf(cur));
-                if( sortedArray[j].indexOf(cur) == 0 )
-                    return false;
+            if( phone_book[i + 1].indexOf(phone_book[i]) == 0 ){
+                return false;
             }
         }
         return true;
