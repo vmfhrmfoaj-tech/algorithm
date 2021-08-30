0f6ac8b96a6028d28019da7cfd52b0e184981e82
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-08-30 18:08:56 +0900

    A003 해결
    뭔가 찝찝

--- src/a0/A003.java
+++ src/a0/A003.java
@@ -1,8 +1,23 @@
 package a0;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class A003 {
 
     public int solution(String[][] clothes) {
-        return 0;
+
+        Map<String, Integer> _clothes = new HashMap<String, Integer>();
+        for(String[] key : clothes){
+            int count = _clothes.getOrDefault(key[1], 0) + 1;
+            _clothes.put(key[1], count);
+        }
+
+        int result = 1;
+        for(String key : _clothes.keySet()){
+            result *= _clothes.get(key) + 1;
+        }
+
+        return result - 1;
     }
 }
