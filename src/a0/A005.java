803e8fe8783f23880a0d23b8b02beef909f78bb1
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2021-09-03 14:44:08 +0900

    기능개발 완료

--- src/a0/A005.java
+++ src/a0/A005.java
@@ -1,9 +1,44 @@
 package a0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class A005 {
 
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
+
+        int len = progresses.length;
+
+        int[] done = new int[len];
+        for(int i=0; i<len; i++){
+            done[i] = deployDay(progresses[i], speeds[i]);
+            // System.out.println( progresses[i] + " + (" + speeds[i] + " * " + done[i] + ") => " + (progresses[i] + ( speeds[i] * done[i])));
+        }
+
+        List<Integer> deploy = new ArrayList<>();
+        for(int cur=0; cur<len; cur++){
+            int day = done[cur];
+            int cnt = 1;
+            for( int j=cur+1; j<len; j++){
+                if( day < done[j]){
+                    break;
+                }
+                cnt++;
+                cur++;
+            }
+            // System.out.println( day + " : " + cnt);
+            deploy.add(cnt);
+        }
+
+        return deploy.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    int deployDay(int progress, int speed){
+        int day = 1;
+        while((progress + (speed * day) < 100)){
+            day++;
+        }
+
+        return day;
     }
 }
